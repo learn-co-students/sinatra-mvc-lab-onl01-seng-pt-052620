1cf6d28c34734d2a81db470274f4a4918c9c470f
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-08-21 23:23:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/" do 
+        erb :user_input 
+    end 
+
+    post "/piglatinize" do 
+        @analyzed_text = PigLatinizer.new(params[:user_input])
+        erb :user_input   
+    end 
 end
+
+
+
+  
+
+"noodle soup" "oodlenay oupsay"
+"flatiron school" "atironflay oolschay "
---
+++ models/piglatinizer.rb
@@ -0,0 +1,10 @@
+class PigLatinizer
+    attr_accessor :text 
+    
+    def initialize
+        @text = text 
+    end 
+
+end 
+
+
